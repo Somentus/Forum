a09f972051c79eb0e5da24091b6cfc99b476bce7
Somentus <somentus@gmail.com>
2017-10-11 13:54:19 +0200

    Make first layout

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE HTML>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Functional Forum</title>
+
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+
+  <h1>Hi there!</h1>
+</body>
+</html>
