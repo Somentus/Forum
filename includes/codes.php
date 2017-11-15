e61debfdce79c807901f5a036c4e4ea20a0b8849
Somentus <somentus@gmail.com>
2017-11-15 03:20:21 +0100

    Prepare to merge with master

--- includes/codes.php
+++ includes/codes.php
@@ -2,6 +2,27 @@
 
 require_once('classes/database.php');
 
+function navbar() {
+	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true ) {
+		// Return navbar for logged in user
+		// TODO: Add navbar for logged in user
+		return '
+		<div id="navbar">
+			<form action="logout.php" method="POST" >
+    			<input type="submit" name="submit" value="Log Out" />
+			</form>
+	  	</div>';
+	} else {
+		// Return navbar for guest
+		return '
+		<div id="navbar">
+	  		<button name="loginTest" onclick="togglePortal(\'login\');" >Login Test</button>
+		  	<button name="registerTest" onclick="togglePortal(\'register\');" >Register Test</button>
+		  	<button name="registerTest" onclick="togglePortal(\'close\');" >X</button>
+	  	</div>';
+	}
+}
+
 function login() {
 	$errors = [];
 
@@ -109,19 +130,4 @@ function body($type) {
 		</div>
 		';
 	}
-
-	// Echo Forum content
-	$categories = Database::query("SELECT * FROM categories ");
-
-	echo "
-	<div id='categories' >
-	";
-
-	foreach($categories as $category) {
-		
-	}
-	
-	echo "
-	</div>
-	";
 }
