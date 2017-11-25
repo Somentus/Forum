f7e32f8142a94842d746f5fa619eaff669b1fa0e
Somentus <somentus@gmail.com>
2017-11-25 15:00:59 +0100

    WIP DB

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once('classes/Database.php');
+require_once('classes/DB.php');
+
+
+
 $query = Database::query("INSERT INTO topics (id, title, forum_id, user_id) VALUES (:id, :title, :forum_id, :user_id)", ['id' => NULL, 'title' => 'Hoi', 'forum_id' => 11, 'user_id' => 2]);
 // $title_id = $query['id'];
 
